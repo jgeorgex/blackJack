db48d45e2c71e0a0a34a352bcd4d57d3f42e1b2f
Julian George <jules104@gmail.com>
2019-11-19 14:47:16 +0000

    Add html and dealt hand hardcoded

---
+++ blackJack.js
@@ -0,0 +1,8 @@
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+console.log("It's Blackjack time!");
+
+console.log("Dealer gives you: ");
+console.log(" " + card1);
+console.log(" " + card2);
